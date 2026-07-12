2d50631cd4e16f19ff375d6a640b5097a35870fa
Jakub Kwantowy <jakob2016k@gmail.com>
2026-07-12 18:33:43 +0200

    General improvements

--- server.js
+++ server.js
@@ -1,25 +1,10 @@
 const express = require('express'); // express, used for webserver actions
 const path = require('path'); // Wait, what?
-const fs = require('fs'); // Filesystem actions
 
 const app = express();
 const HTTP_PORT = 7277; // WYSI
 
-function readFile(filename) {
-    const filepath = path.join(__dirname, filename);
-    const data = fs.readFileSync(filepath);
-    return data
-}
-
-app.get('/', async (req, res) => {
-  res.setHeader('content-type', 'text/html');
-  res.send(readFile("index.html"));
-});
-
-app.get('/startendo.png', async (req, res) => {
-  res.setHeader('content-type', 'image/png');
-  res.send(readFile("startendo.png"));
-});
+app.use(express.static(path.join(__dirname, 'www')))
 
 app.listen(HTTP_PORT, () => {
   console.log(`Web server running on port ${HTTP_PORT}`);
